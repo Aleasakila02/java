163206487c59b2a8102921fda7414cd47f02c8ee
Aleasakilaa <sakilaalea2@gmail.com>
2025-11-24 15:27:53 +0700

    Add files via upload

---
+++ isi.js
@@ -0,0 +1,7 @@
+document.writeln("<center>");
+document.writeln("<hr width=600 color=pink>");
+document.writeln("<h1> SMK ALGHIFARI BANYURESMI</h1>");
+document.writeln("<h2> REKAYASA PERANGKAT LUNAK</h2>");
+document.writeln("<h3> JURUSAN RPL</h3>");
+document.writeln("<hr width=600 color=pink>");
+document.writeln("</center>");
